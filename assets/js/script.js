74d9260fb27ff153a29442d8e065d411fb2561a3
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-03-02 23:39:04 -0500

    finalized work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,48 +3,50 @@ var gameEl = document.querySelector('#game');
 var endEl = document.querySelector('#end');
 var questionsEl = document.querySelector('#questions');
 var titleEl = document.querySelector('#title')
-var isEndGame = false;
+
+
 
 var beginBtn = document.querySelector('#begin');
 var initialsInput = document.querySelector('#initials');
 // declared the variable timer
 var timer;
+var seconds = 90;
 
 var timeEl = document.querySelector('.time')
 var mainEl = document.querySelector('.main')
 var position = 0; //index postion of whatever page you are on
-var score = 100;
-var seconds = 90;
+
+
 
 var questions = [
     {
-        text: ['How much wood could a woodchuck chuck?'],
-        possible: ['Answer 23', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correct: 'Answer 4',
+        text: ['What is primitive data types?'],
+        possible: ['String', 'Numbers', 'Booleans', 'All of the above'],
+        correct: 'All of the above',
     },
     {
-        text: ['meats or poatoes'],
-        possible: ['Answer 24', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correct: 'Answer 4',
+        text: ['What does || mean in Javascript?'],
+        possible: ['Yes', 'Both', 'Or', 'I dont know'],
+        correct: 'Or',
     },
     {
-        text: ['chicken or steak?'],
-        possible: ['Answer 25', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correct: 'Answer 4',
+        text: ['What are the two values of a boolean?'],
+        possible: ['Sun and sky', 'True', 'False', 'True and False'],
+        correct: 'True and False',
     },
     {
-        text: ['cheese or queso'],
-        possible: ['Answer 25', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correct: 'Answer 4',
+        text: ['How do you name a variable?'],
+        possible: ['Var', 'Car', 'Mar', 'Zar'],
+        correct: 'Var',
     },
     {
-        text: ['dogs or cats'],
-        possible: ['Answer 25', 'Answer 2', 'Answer 3', 'Answer 4'],
-        correct: 'Answer 4',
+        text: ['Bonus: What is our instructors name?'],
+        possible: ['Kim K', 'Anthony', 'Pete D', 'Ye-Kanye West'],
+        correct: 'Anthony',
     },
 ];
 
-
+// screen displays .. what happens at each screen
 // //instructor provided
 function startingScreen() {
     startEl.style.display = 'block';
@@ -74,27 +76,23 @@ function loadQuestion() {
     var question = questions[position];
     questionsEl.innerHTML = '';
     titleEl.textContent = question.text;
+
     for (var i = 0; i < question.possible.length; i++) {
         var item = question.possible[i];
         var answerBtn = document.createElement('button');
+        answerBtn.value = item;
         answerBtn.textContent = i + 1 + '. ' + item;
         questionsEl.appendChild(answerBtn);
     }
 }
 function checkAnswer(choice) {
     //check if they got the right answer
-    var correctAnswer = quizQuestions[questionIndex].correct
-    if (choice !== correctAnswer) {
-        // tell them it's wrong
-        rightOrWrong.textContent = 'WRONG!';
+    var correctAnswer = questions[position].correct
+    //checking that there is a value in choice
+    if (choice.length > 0 && choice !== correctAnswer) {
         // subtract 10 sec from timer
-        secondsLeft = Math.max(secondsLeft - 15, 0);
+        seconds -= 10;
     }
-    else {
-        // correct answer!
-        rightOrWrong.textContent = 'CORRECT!';
-    }
-    resultDiv.style.display = "block";
 }
 //instructor provided
 function endGame() {
@@ -107,12 +105,13 @@ function endGame() {
 
 }
 //this is so you can save the scores in local storage..DO NOT COPY COMPlETELY.
-function handleInitialSubmit(event) {
+function handleSubmit(event) {
     event.preventDefault();
-
+//storing the final score in local storage in which seconds left is you score
     var stored = JSON.parse(localStorage.getItem('highScores')) || [];
     var updatedScores = stored.concat({
-        score: score,
+        //in local storage you will see time remaining in seconds which is your score
+        score: `${seconds} second`,
         initials: initialsInput.value
     });
 
@@ -123,22 +122,12 @@ function init() {
     startingScreen();
 }
 
-function handleAnswerClick(event) {
-    if (event.target.matches('button')) {
-        console.log(event.target);
-        position++;
-        if (position < questions.length) {
-            loadQuestion();
-        } else {
-            endGame();
-        }
-    }
-}
-
+ 
 beginBtn.addEventListener('click', questionScreen);
+
 gameEl.addEventListener('click', function (event) {
     if (event.target.matches('button')) {
-        console.log(event.target);
+        checkAnswer(event.target.value);
         position++;
         if (position < questions.length) {
             loadQuestion();
@@ -147,10 +136,6 @@ gameEl.addEventListener('click', function (event) {
         }
     }
 });
-endEl.addEventListener('submit', handleInitialSubmit);
+endEl.addEventListener('submit', handleSubmit);
 init();
 
-//how to adjust score
-//how to do the points for score
-//how to add a timer that subtracts time
-// how to determine if your answer is wronf or right
